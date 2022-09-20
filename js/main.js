a40baa9fbdabb59e7a9aea7268d4ffcde87b4a74
Karen Vardanian <karenvardanian@MacBook-Air-Karen.local>
2022-09-20 19:46:51 +0300

    some changes in the project

--- js/main.js
+++ js/main.js
@@ -24,3 +24,30 @@ document.addEventListener('DOMContentLoaded', function () {
     },
   });
 });
+
+//text animation
+const text = document.querySelector('.text-animation');
+
+text.innerHTML = text.textContent.replace(/\S/g, '<span>$&</span>');
+
+text.addEventListener('mouseover', e => {
+  if (e.target.tagName !== 'SPAN') return;
+  e.target.classList.add('activeText');
+});
+
+//swiper JS
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  slidesPerGroup: 1,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
